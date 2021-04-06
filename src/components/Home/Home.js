d604c8f9ddb86a4fe9f93ae597a3b0fe3f70f9d2
saif.ullah <saif.ullah@meddo.in>
2021-04-06 16:02:59 +0530

    cleanup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,13 @@
 //dependencies
 import React from "react";
-import { Link } from "react-router-dom";
-
-//actions
-import { logout } from "../../actions/authActions";
 
 //imports
 import Heading from "./Heading";
 
 const Home = () => {
-  //onClick handler
-  const onClickHandler = () => {
-    logout();
-  };
-
   return (
     <div>
       <Heading />
-      <button onClick={onClickHandler}>Logout</button>
-      <Link to="/signup">
-        <p>Signup</p>
-      </Link>
     </div>
   );
 };
